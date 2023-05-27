8de3457f7cf52e30df319ca23140da12a8bf33f0
Onur <ob-vest@hotmail.com>
2023-05-27 18:35:21 +0200

    added typescript

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+const pool = new pg.Pool();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/movies", async (req, res) => {
+  console.log(`getting movie`);
+  try {
+    const { rows } = await pool.query("SELECT * FROM movie LIMIT 1");
+    res.send(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
