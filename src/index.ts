564052829f37f730a97c04fcef1ded4049b6dc36
Onur <ob-vest@hotmail.com>
2023-06-04 18:59:08 +0200

    added tech path

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,20 @@ app.get("/movies", async (req, res) => {
   }
 });
 
+app.get("/tech", async (req, res) => {
+  console.log(`getting movie`);
+
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM tech ORDER BY release_date"
+    );
+    res.send(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
 app.get("/movies/:id", async (req, res) => {
   const id = req.params.id;
   console.log(`getting movie with ID ${id}`);
@@ -45,6 +59,24 @@ app.get("/movies/:id", async (req, res) => {
     // by using LEFT JOIN it will return all the rows from the movie table and the matching rows from the weblink table. If there is no match, it will leave it out.
     const { rows } = await pool.query(
       "SELECT movie.*,weblink_links.links,video_links.videos FROM movie LEFT JOIN(SELECT movie_id,json_agg(json_build_object('title',title,'url',url,'date_added',date_added))AS links FROM weblink GROUP BY movie_id)AS weblink_links ON movie.id=weblink_links.movie_id LEFT JOIN(SELECT movie_id,json_agg(json_build_object('title',video.title,'description',video.description,'url',video.url))AS videos FROM video GROUP BY movie_id)AS video_links ON movie.id=video_links.movie_id WHERE movie.id=$1",
+      [id]
+    );
+    res.send(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/tech/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(`getting tech with ID ${id}`);
+
+  try {
+    // This query returns a single row, so we use rows[0] and selects all the columns from the movie table and the weblink table
+    // by using LEFT JOIN it will return all the rows from the movie table and the matching rows from the weblink table. If there is no match, it will leave it out.
+    const { rows } = await pool.query(
+      "SELECT tech.*,weblink_links.links,video_links.videos FROM tech LEFT JOIN(SELECT tech_id,json_agg(json_build_object('title',title,'url',url,'date_added',date_added))AS links FROM weblink GROUP BY tech_id)AS weblink_links ON tech.id=weblink_links.tech_id LEFT JOIN(SELECT tech_id,json_agg(json_build_object('title',video.title,'description',video.description,'url',video.url))AS videos FROM video GROUP BY tech_id)AS video_links ON tech.id=video_links.tech_id WHERE tech.id=$1",
       [id]
     );
     res.send(rows[0]);
